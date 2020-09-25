5572823b3049a4f627be7194316cf508a88081ec
Kees <kees.zijlmans@hightechict.nl>
2020-09-25 16:11:01 +0200

    fixex the pub.sh

--- pub.sh
+++ pub.sh
@@ -7,5 +7,5 @@ if [ -z "$SONARQUBE_PROJECT_KEY" ]; then
   echo 1>&2 "error: missing SONARQUBE_PROJECT_KEY environment variable"
   exit 1
 fi
-docker build -t hightechict/sonarrunnerdotnet:latest --build-arg PROJECT_URL=$SONARQUBE_PROJECT_URL --build-arg PROJECT_KEY=SONARQUBE_PROJECT_KEY
+docker build -t hightechict/sonarrunnerdotnet:latest . --build-arg PROJECT_URL=$SONARQUBE_PROJECT_URL --build-arg PROJECT_KEY=SONARQUBE_PROJECT_KEY
 docker push hightechict/sonarrunnerdotnet:latest
